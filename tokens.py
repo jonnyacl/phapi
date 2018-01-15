b4b3a9f257aabe37ef4f4d8d9c671cd44367a1b0
jonnyacl <lea.jonny@gmail.com>
2018-01-15 06:49:40 +0000

    some token modifications

--- tokens.py
+++ tokens.py
@@ -7,20 +7,20 @@ import phapienv.db_talker as db
 def make_token(user, token_type):
     print(user)
     payload = {
-        'some' : 'payload',
-        'id' : str(user['userId']),
-        'firstname' : str(user['firstName']),
-        'lastname' : str(user['lastName']),
-        'email' : str(user['email']),
-        'username' : str(user['userName'])
+        'some': 'payload',
+        'id': str(user['userId']),
+        'firstname': str(user['firstName']),
+        'lastname': str(user['lastName']),
+        'email': str(user['email']),
+        'username': str(user['userName'])
     }
 
     created = datetime.datetime.utcnow()
     secret = str(created)
-    expiry = created.replace(year = created.year + 5)
+    expiry = created.replace(year=created.year + 5)
 
     token = jwt.encode(payload, secret, algorithm='HS256')
     db.insert_item("usertoken", jwt=token, utType=token_type, created=created,
-                                        expiry=expiry, userId=user['userId'])
+                   expiry=expiry, userId=user['userId'])
 
     return token.decode(encoding="utf-8", errors="strict")
